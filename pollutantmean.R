93e83365bf0931c6ba74e9f31df2e04b6577e402
yaship <yashvir.k.prasad@gmail.com>
2015-04-12 01:39:41 -0400

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -25,8 +25,3 @@ pollutantmean <- function(directory, pollutant,id = 1:332) {
 
    return (mean(mean_vector))
 }
-
-
-pollutantmean("specdata", "sulfate", 1:10) == 4.064
-pollutantmean("specdata", "nitrate", 70:72) == 1.706
-pollutantmean("specdata", "nitrate", 23) == 1.281
